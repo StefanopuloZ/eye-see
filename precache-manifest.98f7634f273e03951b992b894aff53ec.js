ac006d11bacff51841773605bcaa69b133f73fe5
Stefan <stefan.deak@computerrock.com>
2020-06-04 12:17:13 +0200

    Updates

---
+++ precache-manifest.98f7634f273e03951b992b894aff53ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594bf817ecac1c6d51e0c1fa0248e8f5",
+    "url": "/eye-see/index.html"
+  },
+  {
+    "revision": "293c4ee1f9c04c57c776",
+    "url": "/eye-see/static/css/main.5c5baa86.chunk.css"
+  },
+  {
+    "revision": "a7c7082fdc75d57ac38b",
+    "url": "/eye-see/static/js/2.e4d5ffe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eye-see/static/js/2.e4d5ffe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293c4ee1f9c04c57c776",
+    "url": "/eye-see/static/js/main.0e64a7bb.chunk.js"
+  },
+  {
+    "revision": "60eaa5f2ab90bf069b12",
+    "url": "/eye-see/static/js/runtime-main.2e4fb9b0.js"
+  }
+]);
